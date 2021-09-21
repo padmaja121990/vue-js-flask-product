e4ba305758204d10975cdd598ec15759c291663e
Padmaja Ramesh <62902780+padmaja125@users.noreply.github.com>
2021-09-20 23:12:04 -0400

    aggrid get api

---
+++ src/components/agUser.vue
@@ -0,0 +1,44 @@
+<template>
+    <ag-grid-vue style="width: 500px; height: 500px;"
+        class="ag-theme-alphine"
+        :columnDefs="columnDefs"
+        :rowData="rowData">
+    </ag-grid-vue>
+</template>
+
+<script>
+import { AgGridVue } from "ag-grid-vue";
+import Vue from "vue";
+
+export default{
+    name:'agUser',
+    data(){
+        return{
+            columnDefs: null,
+            rowData: null
+        }
+    },
+    components:{
+        AgGridVue
+    },
+    beforeMount() {
+        this.columnDefs = [
+            { field: 'id' },
+            { field: 'title' },
+            { field: 'completed' }
+        ];
+
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(result => result.json())
+            .then(rowData => this.rowData = rowData);
+    }                     
+}
+</script>
+
+<style scoped>
+
+
+    .grid {
+        height: 255px;
+    }
+</style>
